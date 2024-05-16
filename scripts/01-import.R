b1e112d03ab767cb4260aec5dac345fdc123815f
LisaScherrer <lisa.scherrer@stud.unilu.ch>
2024-05-16 12:30:49 +0200

    editing and restructuring import code

--- scripts/01-import.R
+++ scripts/01-import.R
@@ -1,21 +1,21 @@
+# Load Libraries -----------------
 library(httr)
 library(jsonlite)
 library(dplyr)
-#res = GET("https://api.crossref.org/works?query.affiliation=universit%C3%A4t+luzern&filter=type:journal-article")
 
+# Retrieving Crossref Data ------------
+## Creating an empty storage list-----------
 requests1 <- list()
-for(x in 0:276) {
-  req = GET(paste(sep="","https://api.crossref.org/works?query.affiliation=universit%C3%A4t+luzern&filter=type:journal-article&rows=1000&offset=", x*1000))
+
+## For Loop Data Request -------------
+for(x in 0:276) { #there are 276000 records in total
+  req = GET(paste(sep="","https://api.crossref.org/works?query.affiliation=universit%C3%A4t+luzern&filter=type:journal-article&rows=1000&offset=", x*1000)) 
+  #only 1000 records per request
   v <- rawToChar(req$content)
   print(paste("added request with offset:", x))
   print(paste(" len=",nchar(v)))
  if (nchar(v) < 10000) break;
   
  requests1[length(requests1)+1]=v
-  save(requests1, file = "crossref.Rda")
+  save(requests1, file = "crossref.Rda") #save the data after every request
 }
-
-#rawToChar(
-#View(requests1[1])
-#data = fromJSON(requests1[[1]], flatten = TRUE)
-#data = fromJSON(rawToChar(res$content), flatten = TRUE)
