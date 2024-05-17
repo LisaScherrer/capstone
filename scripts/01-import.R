24524f9ef5d8de73e5ad0f01ddad4220612c9cbd
LisaScherrer <lisa.scherrer@stud.unilu.ch>
2024-05-17 16:41:08 +0200

    commenting code

--- scripts/01-import.R
+++ scripts/01-import.R
@@ -8,15 +8,24 @@ library(dplyr)
 ## Creating an Empty List -----------
 requests1 <- list()
 
-## For Loop Data Request -------------
-for(x in 0:276) { #there are 276000 records in total
+## Main Loop -------------
+  # Request for a total of 276000 records
+for(x in 0:276) {
+  
+  # GET request for each 1000 records
   req = GET(paste(sep="","https://api.crossref.org/works?query.affiliation=universit%C3%A4t+luzern&filter=type:journal-article&rows=1000&offset=", x*1000)) 
-  #only 1000 records per request
+ 
+  # Convert raw data to character
   v <- rawToChar(req$content)
   print(paste("added request with offset:", x))
   print(paste(" len=",nchar(v)))
+  
+  # Break the loop if the character length is less than 10000
  if (nchar(v) < 10000) break;
   
+  # Add the request to the list
  requests1[length(requests1)+1]=v
+  
+  # Save the data after every request
   save(requests1, file = "crossref.Rda") #save the data after every request
 }
