fef6e4960410954ecf66e591cc1b16f5170c507c
LisaScherrer <lisa.scherrer@stud.unilu.ch>
2024-05-17 16:50:41 +0200

    organise join script

--- scripts/02-join.R
+++ scripts/02-join.R
@@ -1,16 +1,23 @@
 # Load Packages ---------
 library(dplyr)
+library(jsonlite)
 
 # Load Data --------------
 load("crossref.Rda")
 
 # Join Data Frames -------------
+## Creating an Empty List ------------
 df_journal <- data.frame()
+
+## Main Loop ------------
 for(x in 1:length(requests1)){
-data <- fromJSON(requests1[[x]], flatten = TRUE)
-df1 <- data.frame(data$message$items)
-df_journal <- bind_rows(df_journal, df1)
+  # Process Request Data 
+  data <- fromJSON(requests1[[x]], flatten = TRUE)
+  df1 <- data.frame(data$message$items)
+  
+  # Join Data Frames
+  df_journal <- bind_rows(df_journal, df1)
 }
 
-#Save Joined Data Frame --------------
+# Save Joined Data Frame --------------
 save(df_journal, file = "df_journal.Rda")
